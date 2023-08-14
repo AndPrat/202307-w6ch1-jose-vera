727b3ea8d32b49253122255092009fe254b436bc
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-14 16:52:36 +0200

    Create custom hook to get films data

---
+++ src/hooks/useFilmsApi.ts
@@ -0,0 +1,21 @@
+import { useCallback } from "react";
+import Film from "../types";
+
+const useFilmsApi = () => {
+  const apiUrl = `${import.meta.env.VITE_FILMS_API_URL}/films`;
+
+  const getFilmsApi = useCallback(async () => {
+    try {
+      const response = await fetch(apiUrl);
+      const apiFilms = (await response.json()) as Film[];
+
+      return apiFilms;
+    } catch (error) {
+      throw new Error("Cant show any film");
+    }
+  }, [apiUrl]);
+
+  return { getFilmsApi };
+};
+
+export default useFilmsApi;
