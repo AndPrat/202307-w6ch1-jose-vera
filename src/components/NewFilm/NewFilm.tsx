1a61309ea1e640055bc556db883b218be40c33cb
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-15 16:35:18 +0200

    Add new useState on NewFilm formular

--- src/components/NewFilm/NewFilm.tsx
+++ src/components/NewFilm/NewFilm.tsx
@@ -1,27 +1,70 @@
 import { useState } from "react";
+import Film from "../../types";
 import Button from "../Button/Button";
 import "./NewFilm.css";
 
-const NewFilm = (): React.ReactElement => {
+const NewFilmForm = (): React.ReactElement => {
   const [canSubmit] = useState(false);
 
+  const initialFilmData = {
+    id: 0,
+    title: "",
+    poster: "",
+    director: "",
+    year: 0,
+  };
+
+  const [newFilm, setNewFilm] = useState<Film>(initialFilmData);
+
+  const changeNewFilm = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewFilm((newFilm) => ({
+      ...newFilm,
+      [event.target.id]: event.target.type,
+    }));
+  };
+
   return (
-    <form className="form-film">
+    <form
+      className="form-film"
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
       <div className="form-control">
         <label htmlFor="title">Título: </label>
-        <input type="text" id="title" />
+        <input
+          type="text"
+          id="title"
+          value={newFilm.title}
+          onChange={changeNewFilm}
+        />
       </div>
       <div className="form-control">
         <label htmlFor="director">Dirección: </label>
-        <input type="text" id="director" />
+        <input
+          type="text"
+          id="director"
+          value={newFilm.director}
+          onChange={changeNewFilm}
+        />
       </div>
       <div className="form-control">
         <label htmlFor="year">Año: </label>
-        <input type="number" id="year" />
+        <input
+          type="number"
+          id="year"
+          value={newFilm.year}
+          onChange={changeNewFilm}
+        />
       </div>
       <div className="form-control">
         <label htmlFor="poster">URL cartel: </label>
-        <input type="url" id="poster" />
+        <input
+          type="url"
+          id="poster"
+          value={newFilm.poster}
+          onChange={changeNewFilm}
+        />
       </div>
       <div className="form-control">
         <Button disabled={!canSubmit}>Crear película</Button>
@@ -30,4 +73,4 @@ const NewFilm = (): React.ReactElement => {
   );
 };
 
-export default NewFilm;
+export default NewFilmForm;
--- src/pages/NewFilmPage/NewFilmPage.tsx
+++ src/pages/NewFilmPage/NewFilmPage.tsx
@@ -1,10 +1,10 @@
-import NewFilm from "../../components/NewFilm/NewFilm";
+import NewFilmForm from "../../components/NewFilm/NewFilm";
 
 const NewFilmPage = (): React.ReactElement => {
   return (
     <>
       <h2>Añadir nueva película</h2>
-      <NewFilm />
+      <NewFilmForm />
     </>
   );
 };
